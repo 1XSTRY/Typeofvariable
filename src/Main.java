2b70eb03e8494f2b7130aceb6488df0e6da2e2d6
1XSTRY <kosogornov99@gmail.com>
2023-09-17 18:58:26 +0500

    Declared variables of different types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        //task 1
+        //Integer type
+        byte a = 45;
+       short b = 20000;
+       int c = 100000;
+       long d = 30000000;
+        System.out.println("Значение переменной a с типом byte равно " + a);
+        System.out.println("Значение переменной b с типом short равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        //Floating Point Types
+        float pi = 3.14f;
+        double e = 4.54064369;
+        System.out.println("Значение переменной pi с типом float равно " + pi);
+        System.out.println("Значение переменной e с типом double равно " + e);
+
+
+
 
     }
 }
